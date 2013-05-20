99879fddc1001a8fb273008f655db409e1b0a61c
Steven <theepicsnail@gmail.com>
2013-05-20 01:37:50 -0400

    Added Cell structure

--- maze.go
+++ maze.go
@@ -18,10 +18,21 @@ func getCh() (byte, error){
     return 0, err
 }
 
+type Cell struct {
+    rune rune
+    seen bool
+}
+
+func (cell *Cell) getRune() rune {
+    if cell.seen {
+        return cell.rune
+    }
+    return rune('#')
+}
 
 type Maze struct { 
     rows, cols int
-    data [][] byte
+    data [][] Cell
 }
 
 func (maze *Maze) String() string {
@@ -29,34 +40,38 @@ func (maze *Maze) String() string {
 
     for row := range maze.data {
         for col := range maze.data[row] {
-            buffer.WriteByte(maze.data[row][col])
+            buffer.WriteRune(maze.runeAt(row, col))
         }
         buffer.WriteByte('\n')
     }
     return buffer.String()
 }
 
+func (maze *Maze) runeAt(row, col int) rune {
+    return maze.data[row][col].rune
+}
+
 func (maze *Maze) getNeighbors(pos Position) []Position {
     var out []Position
-    if pos.row-2 > 0 && maze.data[pos.row-2][pos.col] == '#'{
+    if pos.row-2 > 0 && maze.runeAt(pos.row-2,pos.col) == '#'{
         out = append(out, Position{pos.row-2, pos.col})
     }
-    if pos.row+2 < maze.rows && maze.data[pos.row+2][pos.col] == '#'{
+    if pos.row+2 < maze.rows && maze.runeAt(pos.row+2,pos.col) == '#'{
         out = append(out, Position{pos.row+2, pos.col})
     }
-    if pos.col-2 > 0 && maze.data[pos.row][pos.col-2] == '#'{
+    if pos.col-2 > 0 && maze.runeAt(pos.row,pos.col-2) == '#'{
         out = append(out, Position{pos.row, pos.col-2})
     }
-    if pos.col+2 < maze.cols && maze.data[pos.row][pos.col+2] == '#'{
+    if pos.col+2 < maze.cols && maze.runeAt(pos.row,pos.col+2) == '#'{
         out = append(out, Position{pos.row, pos.col+2})
     }
     return out
 }
 
 func (maze *Maze) connect(p1, p2 Position) {
-   maze.data[p1.row][p1.col] = ' ' 
-   maze.data[(p1.row+p2.row)>>1][(p1.col+p2.col)>>1] = ' '
-   maze.data[p2.row][p2.col] = ' ' 
+   maze.data[p1.row][p1.col].rune = ' '
+   maze.data[(p1.row+p2.row)>>1][(p1.col+p2.col)>>1].rune = ' '
+   maze.data[p2.row][p2.col].rune = ' ' 
 }
 
 func NewMaze(rows, cols int) *Maze {
@@ -65,11 +80,11 @@ func NewMaze(rows, cols int) *Maze {
     rows= rows* 2 + 1 //
     m.rows = rows
     m.cols = cols
-    m.data = make([][]byte, rows)
+    m.data = make([][]Cell, rows)
     for row:= range m.data {
-        m.data[row] = make([]byte, cols)
+        m.data[row] = make([]Cell, cols)
         for col := range m.data[row] {
-            m.data[row][col] = '#'
+            m.data[row][col].rune = '#'
         }
     }
     return m
@@ -109,3 +124,4 @@ func main() {
     }
     fmt.Println(maze)
 }
+// ☺
